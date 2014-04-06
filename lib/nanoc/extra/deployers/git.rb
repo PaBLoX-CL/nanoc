aae98c8e901ff029191814c7f9708271e4842903
Lifepillar <github@lifepillar.com>
2014-04-06 11:24:17 +0200

    Use IO.popen instead of backticks.

--- lib/nanoc/extra/deployers/git.rb
+++ lib/nanoc/extra/deployers/git.rb
@@ -1,5 +1,26 @@
 # encoding: utf-8
 
+# Copyright (c) 2012 Tom Vaughan <thomas.david.vaughan@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 module Nanoc::Extra::Deployers
 
   # A deployer that deploys a site using [Git](http://git-scm.com).
@@ -27,7 +48,7 @@ module Nanoc::Extra::Deployers
 
       # Check if remote is not a git url
       unless remote =~ /\.git$/
-        remote = `git config --get remote.#{remote}.url`.chop
+        remote = IO.popen(['git', 'config', '--get', "remote.#{remote}.url"]) { |c| c.read }.chop
       end
 
       # If the remote name doesn't exist in the main repo
