ce4d64a1a0d8c61eabf88d3e3188dc564340b49e
Lifepillar <github@lifepillar.com>
2014-04-18 12:24:58 +0300

    Update copyright notice.

--- lib/nanoc/extra/deployers/git.rb
+++ lib/nanoc/extra/deployers/git.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-# Copyright (c) 2012 Tom Vaughan <thomas.david.vaughan@gmail.com>
+# Portions Copyright (c) 2012 Tom Vaughan <thomas.david.vaughan@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
